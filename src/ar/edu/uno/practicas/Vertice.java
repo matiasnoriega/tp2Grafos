3352f5cbe8430692ae878a8983c0791f69cf450e
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2016-06-23 14:13:03 -0300

    Update Vertice.java

--- src/ar/edu/uno/practicas/Vertice.java
+++ src/ar/edu/uno/practicas/Vertice.java
@@ -3,20 +3,27 @@ package ar.edu.uno.poo2.modulo;
 import java.util.ArrayList;
 
 public class Vertice {
+	private Integer grado;
+	
 	private ArrayList<Arista> aristas;
 	private String nombre;
 	
 	public Vertice() {
 	}
 	
+	//getters y setters
+	public Integer getGrado() {
+		return grado;
+	}
+	public void setGrado(Integer grado) {
+		this.grado = grado;
+	}
 	private void setNombre(String n) {
 		this.nombre = n;
 	}
-	
 	public String getNombre() {
 		return this.nombre;
 	}
-	
 	public void AddArista(Arista a) {
 		aristas.add(a);
 	}
