de575fbe74f3e23f43aaacc3fb68f0eb80b007c5
Maluc8 <maluc8@gmail.com>
2016-06-24 17:07:22 -0300

    Create Test.java

---
+++ src/ar/edu/uno/practicas/Test.java
@@ -0,0 +1,38 @@
+package 
+import java.io.*;
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Grafo grafo;
+		File file= new File("grafo.in");
+		FileReader fr = null;
+		BufferedReader br;
+		
+		
+		try{
+			fr= new FileReader(file);
+			br= new BufferedReader(fr);
+			StringTokenizer linea= new StringTokenizer(br.readLine());
+			int aristas= Integer.parseInt(linea.nextToken());
+			int porcentajeAdyacencia= Integer.parseInt(linea.nextToken());
+			grafo= new Grafo(aristas, porcentajeAdyacencia);
+		}catch (FileNotFoundException e){
+			System.out.println(file.getAbsolutePath());
+			System.out.println("Error Al abrir el archivo");
+		}catch (Exception e1){
+			System.out.println(e1.getStackTrace());
+		}
+		finally{try{                    
+			if( null != fr ){   
+				fr.close();     
+			}                  
+		}catch (Exception e2){ 
+			System.out.println(e2.getStackTrace());
+		}	
+		}
+		// Esperando el toString()...
+	}
+
+}
